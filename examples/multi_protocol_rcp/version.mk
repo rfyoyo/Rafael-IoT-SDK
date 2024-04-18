a97f24c32c66f9640793f02be95b0c001fb9e689
Rex <rex.huang@rafaelmicro.com>
2024-04-18 11:26:00 +0800

    Update version to 3.4.0

--- examples/multi_protocol_rcp/version.mk
+++ examples/multi_protocol_rcp/version.mk
@@ -1,4 +1,4 @@
-PROJ_VER := 3.3.0-
+PROJ_VER := 3.4.0-
 GIT_HASH := $(shell cd ${RAFAEL_IOT_SDK_PATH} && git describe --always --dirty)
 EXTRA_CPPFLAGS ?=
 EXTRA_CPPFLAGS += -D RAFAEL_SDK_VER=\"$(PROJ_VER)$(GIT_HASH)\"
